058f12240a70534ae36558f2a115aa870521a97e
aseid1997 <ibnuseid376@gmail.com>
2022-05-24 08:39:03 +0300

    abdulkerim

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -2,16 +2,23 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ClassComponent } from './class/class.component';
 import { CourseComponent } from './course/course.component';
+import { HomeComponent } from './home.component';
 import { StudentComponent } from './student/student.component';
 import { TestComponent } from './test/test.component';
 
 
 const routes: Routes = [
-  { path: 'student', component: StudentComponent },
-  { path: 'class', component: ClassComponent },
-  { path: 'course', component: CourseComponent },
-  { path: 'test', component: TestComponent },
-  
+  {
+    path: '', 
+    children: [
+      { path: 'student', component: StudentComponent },
+      { path: 'class', component: ClassComponent },
+      { path: 'course', component: CourseComponent },
+      { path: 'test', component: TestComponent },
+
+    ]
+  }
+
 ];
 
 @NgModule({
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { ClassDialogComponent } from './class-dialog/class-dialog.component';
+import { CourseDialogComponent } from './course-dialog/course-dialog.component';
 import { StudentDialogComponent } from './student-dialog/student-dialog.component';
+import { TestDialogComponent } from './test-dialog/test-dialog.component';
 
 @Component({
   selector: 'app-home',
@@ -18,6 +20,16 @@ export class HomeComponent implements OnInit {
   }
   openClassDialog() {
     this.dialog.open(ClassDialogComponent, {
+      width: '50%',
+    });
+  }
+  openCourseDialog() {
+    this.dialog.open(CourseDialogComponent, {
+      width: '50%',
+    });
+  }
+  openTestDialog() {
+    this.dialog.open(TestDialogComponent, {
       width: '50%',
     });
   }
